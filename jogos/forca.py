70ce5a949b62c51e5d9c3fc4e314228f1459bf13
Anna Livia <apradolsv@gmail.com>
2021-08-30 15:18:31 -0300

    alteracoes no jogo

--- jogos/forca.py
+++ jogos/forca.py
@@ -1,7 +1,7 @@
 def jogo():
     print('Bem-vindo ao jogo de forca!')
 
-    palavra = 'nana'
+    palavra = 'nana'.upper()
     letras_certas = ['_', '_', '_', '_']
 
     enforcou = False
@@ -13,23 +13,26 @@ def jogo():
     while(not enforcou and not acertou):
 
         chute = input('Digite uma letra:')
-        chute = chute.strip()
+        chute = chute.strip().upper()
 
         if(chute in palavra):
             posicao = 0
             for letra in palavra:
-                if(chute.upper() == letra.upper()):
+                if(chute == letra):
                     letras_certas[posicao] = letra
-                posicao = posicao + 1
+                posicao += 1
 
         else:
-            erro = erro + 1
+            erro += 1
 
         enforcou = erro == 5
-
+        acertou = '_' not in letras_certas
         print(letras_certas)    
-        #print('jogando...')
-
+        
+    if(acertou):
+        print("Você ganhou")
+    else:
+        print('Você perdeu')
     print('Fim de jogo!!!')
 
 if(__name__ == "__main__"):
